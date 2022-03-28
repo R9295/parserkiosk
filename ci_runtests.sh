6a032947158349db6a0bbcbc12d56c69e4ac5ee7
aarnav <aarnavbos@gmail.com>
2022-03-28 17:56:04 +0200

    CI: Fix lcov path

--- ci_runtests.sh
+++ ci_runtests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-mkdir coverage
 poetry run coverage run  -m pytest -s tests/
 # coveralls needs lcov, thus forcing a different command
-poetry run coverage lcov -o coverage/coverage.lcov
+poetry run coverage lcov
 
