b8093e209084b6bda8869615c26fc6678d6227d3
aarnav <aarnavbos@gmail.com>
2022-03-29 11:54:35 +0200

    Add tests for JSON example

--- examples/json/tests/commons.py
+++ examples/json/tests/commons.py
@@ -1,3 +1,16 @@
-def compare_dict(a, b):
+def assert_dict(a, b):
     return True
     #    return a == b
+
+
+def assert_list(a, b):
+    asserts = []
+    assert len(a) == len(b)
+    for index, item in enumerate(a):
+        b_item = b[index]
+        asserts.append(item == b_item and type(item) == type(b_item))
+    return all(asserts)
+
+
+def assert_float(a, b):
+    return isinstance(a, float) and isinstance(b, float) and a == b
